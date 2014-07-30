62079b4dd0303634d253f5182f8581ff3a255633
stnik <nikolay_nesterenko@yahoo.com>
2014-07-30 13:39:41 +0300

    Selection Sort

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,8 +1,33 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * Created by nikolay on 30.07.14.
  */
 public class SelectionSort {
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min])
+                    min = j;
+            }
+            if (min!=i){
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{5,8,2,9,4,1,3,6,7};
+        System.out.println(Arrays.toString(a));
+        System.out.println("-----------");
+        SelectionSort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 
 }
